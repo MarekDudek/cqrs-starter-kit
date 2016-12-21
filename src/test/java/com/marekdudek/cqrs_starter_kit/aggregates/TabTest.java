a29f341f0a261bc992a84895f95e17d65b584203
Marek Dudek <marek.dudek@gmail.com>
2016-12-21 22:29:52 +0100

    refactored some more

--- src/test/java/com/marekdudek/cqrs_starter_kit/aggregates/TabTest.java
+++ src/test/java/com/marekdudek/cqrs_starter_kit/aggregates/TabTest.java
@@ -7,6 +7,7 @@ import com.marekdudek.cqrs_starter_kit.events.DrinksOrdered;
 import com.marekdudek.cqrs_starter_kit.events.FoodOrdered;
 import com.marekdudek.cqrs_starter_kit.events.TabOpened;
 import com.marekdudek.cqrs_starter_kit.values.OrderedItem;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -21,65 +22,67 @@ import static org.junit.Assert.assertThat;
 
 public class TabTest {
 
-    // given
+    /**
+     * System under test.
+     */
+    private Tab tab;
 
+    // fields
     private UUID id = UUID.randomUUID();
     private String waiter = "John";
     private int table = 2;
 
+    // values
     private OrderedItem drink1 = new OrderedItem(3, "a beer", true, new BigDecimal(12));
     private OrderedItem drink2 = new OrderedItem(4, "wine", true, new BigDecimal(15));
-
     private OrderedItem food1 = new OrderedItem(5, "stake", false, new BigDecimal(17));
     private OrderedItem food2 = new OrderedItem(6, "salad", false, new BigDecimal(19));
 
+    // commands
+    private OpenTab openTab = new OpenTab(id, table, waiter);
+
+    @Before
+    public void setUp() {
+        tab = new Tab();
+    }
+
     @Test
     public void can_open_a_new_tab() {
-        // given
-        Tab tab = new Tab();
-        OpenTab openTab = new OpenTab(id, table, waiter);
         // when
         Iterable<?> events = tab.handle(openTab);
         // then
-        final Object event = events.iterator().next();
+        Object event = events.iterator().next();
         assertThat(event, is(equalTo(new TabOpened(id, table, waiter))));
     }
 
     @Test(expected = TabNotOpen.class)
     public void cannot_order_with_unopened_tab() {
-        // given
-        Tab tab = new Tab();
         // when
-        PlaceOrder command = new PlaceOrder(id, singletonList(drink1));
-        tab.handle(command);
+        PlaceOrder placeOrder = new PlaceOrder(id, singletonList(drink1));
+        tab.handle(placeOrder);
         // then throws exception
     }
 
     @Test
     public void can_place_drinks_order() {
         // given
-        Tab tab = new Tab();
-        OpenTab openTabCommand = new OpenTab(id, table, waiter);
-        Iterable<?> openEvents = tab.handle(openTabCommand);
-        TabOpened event = (TabOpened) openEvents.iterator().next();
-        tab.apply(event);
+        Iterable<?> openEvents = tab.handle(openTab);
+        tab.apply((TabOpened) openEvents.iterator().next());
         // when
-        PlaceOrder command = new PlaceOrder(id, asList(drink1, drink2));
-        Iterable<?> events = tab.handle(command);
+        PlaceOrder placeOrder = new PlaceOrder(id, asList(drink1, drink2));
+        Iterable<?> orderEvents = tab.handle(placeOrder);
         // then
-        assertThat(events.iterator().next(), is(equalTo(new DrinksOrdered(id, asList(drink1, drink2)))));
+        assertThat(orderEvents.iterator().next(), is(equalTo(new DrinksOrdered(id, asList(drink1, drink2)))));
     }
 
     @Test
     public void can_place_food_order() {
         // given
-        Tab tab = new Tab();
-        OpenTab openTabCommand = new OpenTab(id, table, waiter);
-        Iterable<?> openEvents = tab.handle(openTabCommand);
+        Iterable<?> openEvents = tab.handle(openTab);
         tab.apply((TabOpened) openEvents.iterator().next());
         // when
-        PlaceOrder command = new PlaceOrder(id, asList(food1, food2));
-        Iterable<?> events = tab.handle(command);
+        PlaceOrder placeOrder = new PlaceOrder(id, asList(food1, food2));
+        Iterable<?> events = tab.handle(placeOrder);
         // then
         assertThat(events.iterator().next(), is(equalTo(new FoodOrdered(id, asList(food1, food2)))));
     }
@@ -87,15 +90,13 @@ public class TabTest {
     @Test
     public void can_place_drink_and_food_order() {
         // given
-        Tab tab = new Tab();
-        OpenTab openTabCommand = new OpenTab(id, table, waiter);
-        Iterable<?> openEvents = tab.handle(openTabCommand);
+        Iterable<?> openEvents = tab.handle(openTab);
         tab.apply((TabOpened) openEvents.iterator().next());
         // when
-        PlaceOrder command = new PlaceOrder(id, asList(food1, drink1));
-        Iterable<?> events = tab.handle(command);
+        PlaceOrder placeOrder = new PlaceOrder(id, asList(food1, drink1));
+        Iterable<?> orderEvents = tab.handle(placeOrder);
         // then
-        Iterator<?> iterator = events.iterator();
+        Iterator<?> iterator = orderEvents.iterator();
         Object drinkEvent = iterator.next();
         assertThat(drinkEvent, is(equalTo(new DrinksOrdered(id, singletonList(drink1)))));
         Object foodEvent = iterator.next();
